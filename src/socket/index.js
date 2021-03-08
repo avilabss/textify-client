6b4a97498b11c6df78dce43d79cf2416e5d3855a
git-avinash <mail.avinashsah@gmail.com>
2021-03-09 00:06:39 +0530

    trying drop cli fix

--- src/socket/index.js
+++ src/socket/index.js
@@ -6,6 +6,7 @@ import { UPDATE_OUT_TEXT } from "../redux/actionTypes/outText";
 
 export const socket = io("https://d5182fa0717a.ngrok.io", {
   transports: ["websocket"],
+  upgrade: false,
 });
 
 export const SocketContext = React.createContext();
